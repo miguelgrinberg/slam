cd53f06b25c7042cba5347c9bfbca3b52dccc8bf
Miguel Grinberg <miguelgrinberg50@gmail.com>
2016-12-24 19:27:25 -0800

    first attempt, ultra pre-alpha code

---
+++ slam/handler.py
@@ -0,0 +1,71 @@
+from io import BytesIO
+import sys
+try:
+    from urllib import quote
+except ImportError:
+    from urllib.parse import quote
+
+ 
+def lambda_handler(event, context):
+    """Main entry point for the lambda function.
+
+    This function builds a request using API Gateway's proxy integration
+    event, and invokes the WSGI application with it. The response is then
+    formatted according to the proxy integration requirements.
+    """
+    query_string = None
+    if 'queryStringParameters' in event['input']:
+        query_string = '&'.join(
+            [quote(k) + '=' + quote(v)
+             for k, v in event['input']['queryStringParameters'].items()])
+    headers = event['input'].get('headers', {})
+    body = event['input'].get('body').encode('utf-8')
+    environ = {
+        'REQUEST_METHOD': event['input'].get('httpMethod', 'GET'),
+        'SCRIPT_NAME': '',
+        'PATH_INFO': event['input'].get('path', '/'),
+        'QUERY_STRING': query_string,
+        'SERVER_NAME': '',
+        'SERVER_PORT': 80,
+        'HTTP_HOST': '',
+        'SERVER_PROTOCOL': 'https',
+        'CONTENT_TYPE': headers.get('Content-Type', ''),
+        'CONTENT_LENGTH': headers.get('Content-Length',
+                                      str(len(body)) if body is not None
+                                          else '0'),
+        'wsgi.version': '',
+        'wsgi.url_scheme': '',
+        'wsgi.input': BytesIO(body) if body is not None else None,
+        'wsgi.errors': sys.stderr,
+        'wsgi.multithread': False,
+        'wsgi.multiprocess': False,
+        'wsgi.run_once': True,
+        'lambda.event': event,
+        'lambda.context': context,
+    }
+
+    status_headers = [None, None]
+    body = []
+ 
+    def write(item):
+        body.append(item)
+ 
+    def start_response(status, headers):
+        status_headers[:] = [status, headers]
+        return write
+ 
+    app_iter = lambda_handler.app(environ, start_response)
+    try:
+        for item in app_iter:
+            body.append(item)
+    finally:
+        if hasattr(app_iter, 'close'):
+            app_iter.close()
+
+    status = status_headers[0].split()
+    headers = status_headers[1]
+    return {
+        'statusCode': int(status[0]),
+        'headers': headers,
+        'body': b''.join(body)
+    }
