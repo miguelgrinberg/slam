d116aadef6d90a47ece64bad8c86036c031eccee
Miguel Grinberg <miguel.grinberg@gmail.com>
2017-01-05 19:12:31 -0800

    Release 0.1.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='slam',
-    version='0.1.2',
+    version='0.1.3',
     url='https://github.com/miguelgrinberg/slam/',
     license='MIT',
     author='Miguel Grinberg',
